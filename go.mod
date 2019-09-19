4b275543105bf387d24fa3760917a18ef5339bf2
Igor Tamashchuk <legigor@gmail.com>
2019-09-19 14:47:13 +0300

    Schedule job using in-proc cron

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module telerss
 
 go 1.13
 
-require github.com/sirupsen/logrus v1.4.2
+require (
+	github.com/robfig/cron v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.4.2
+)
--- main.go
+++ main.go
@@ -3,12 +3,29 @@ package main
 import (
 	"time"
 
+	"github.com/robfig/cron"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+
+	c := cron.New()
+
+	// Same as "0 * * * *"
+	err := c.AddFunc("@every 1m", func() {
+		log.Info("Tick from CRON")
+	})
+	if err != nil {
+		log.Fatalf("Error scheduling the job: %w", err)
+	} else {
+		c.Start()
+		for _, e := range c.Entries() {
+			log.Infof("Have a job scheduled at %s", e.Schedule)
+		}
+	}
+
 	for {
-		log.Info("Hello")
+		log.Info("Ping")
 		time.Sleep(3 * time.Second)
 	}
 }
